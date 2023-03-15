d4a5e821d272b937f391b98826a04ec9b53dceb6
Leonard <lenny@erwinefamily.net>
2023-03-14 23:04:51 -0400

    Code cleanup

--- CdnSync/CdnJs/Response/LibraryAllVersions.cs
+++ CdnSync/CdnJs/Response/LibraryAllVersions.cs
@@ -1,30 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Net;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace CdnSync.CdnJs.Response;
 
-// public class LibraryAuthor
-// {
-//     public string name = "";
-//     public string url = "";
-// }
 public class LibraryAllVersions
 {
     public string name = "";
-    // public List<LibraryAuthor>? authors;
     public string? description;
-    // public string? homepage;
-    // public List<string>? keywords;
-    // public string? license;
-    // public string? version;
-    // public string? author;
     public List<string>? versions;
 
     [JsonExtensionData]
