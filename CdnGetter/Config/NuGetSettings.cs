7e71201eb3226387ff1320bcce116711ac3cdddf
erwinel <lerwine@outlook.com>
2023-11-07 17:34:06 -0500

    Refactored settings

--- CdnGetter/Config/NuGetSettings.cs
+++ CdnGetter/Config/NuGetSettings.cs
@@ -1,22 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CdnGetter.Config;
 
 public class NuGetSettings
 {
-    public const string DEFAULT_V3_API_URI = "https://api.nuget.org/v3/index.json";
-    
-    public const string DEFAULT_LOCAL_REPOSITORY = "LocalSource";
-    
     /// <summary>
-    /// Specifies the remote NuGet endpoint URL.
+    /// The default remote endpoint URL for the V3 NGet API.
     /// </summary>
-    /// <remarks>The default value of this setting is defined in the <see cref="DEFAULT_V3_API_URI" /> constant.</remarks>
-    public string? V3ApiUrl { get; set; }
-    
+    public const string DEFAULT_SERVICE_INDEX_URL = "https://api.nuget.org/v3/index.json";
+
+    /// <summary>
+    /// The default path of the local repository, relative to the <see cref="Microsoft.Extensions.Hosting.ContentRootPath" />.
+    /// </summary>
+    public const string DEFAULT_LOCAL_REPOSITORY = "LocalSource";
+
+    /// <summary>
+    /// Specifies the remote endpoint URL for the V3 NGet API.
+    /// </summary>
+    /// <remarks>The default value of this setting is defined in the <see cref="DEFAULT_SERVICE_INDEX_URL" /> constant.</remarks>
+    /// <seealso href="https://learn.microsoft.com/en-us/nuget/api/overview#service-index"/>
+    public string? ServiceIndexUrl { get; set; }
+
     /// <summary>
     /// Specifies the relative or absolute path of the local repository.
     /// </summary>
