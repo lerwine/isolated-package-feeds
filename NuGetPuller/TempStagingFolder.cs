c9849bc6a3729a382801c27abba29ea4127297d0
erwinel <37079914+erwinel@users.noreply.github.com>
2023-12-07 18:57:05 +0000

    Added methods for file and directory creation

--- NuGetPuller/TempStagingFolder.cs
+++ NuGetPuller/TempStagingFolder.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace NuGetPuller;
 
 public sealed class TempStagingFolder : IDisposable
@@ -5,6 +7,148 @@ public sealed class TempStagingFolder : IDisposable
     private DirectoryInfo _directory;
 
     public DirectoryInfo Directory => _directory ?? throw new ObjectDisposedException(nameof(TempStagingFolder));
+
+    public FileInfo WriteNewRandomFileInfo(Action<StreamWriter> writeContent, Encoding? encoding) => WriteNewRandomFileInfo(writeContent, null, encoding);
+
+    public FileInfo WriteNewRandomFileInfo(Action<StreamWriter> writeContent, string? extension = null, Encoding? encoding = null)
+    {
+        FileInfo result;
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        }
+        else
+        {
+            if (extension[0] != '.') extension = $".{extension}";
+            result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+        }
+        if (encoding is null)
+            using (var writer = result.CreateText())
+            {
+                writeContent(writer);
+                writer.Flush();
+            }
+        else
+            using (var stream = result.Create())
+            {
+                using var writer = new StreamWriter(stream, encoding);
+                writeContent(writer);
+                writer.Flush();
+            }
+        result.Refresh();
+        return result;
+    }
+
+    public FileInfo NewRandomFileInfo(string? extension) => NewRandomFileInfo(null, extension);
+
+    public FileInfo NewRandomFileInfo(Action<FileStream>? writeContent = null, string? extension = null)
+    {
+        FileInfo result;
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        }
+        else
+        {
+            if (extension[0] != '.') extension = $".{extension}";
+            result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+        }
+        using (var stream = result.Create())
+        {
+            if (writeContent is not null)
+            {
+                writeContent(stream);
+                stream.Flush();
+            }
+        }
+        result.Refresh();
+        return result;
+    }
+
+    public Task<FileInfo> WriteNewRandomFileInfoAsync(Func<StreamWriter, CancellationToken, Task> writeContentAsync, CancellationToken cancellationToken) => WriteNewRandomFileInfoAsync(writeContentAsync, null, null, cancellationToken);
+
+    public Task<FileInfo> WriteNewRandomFileInfoAsync(Func<StreamWriter, CancellationToken, Task> writeContentAsync, string? extension, CancellationToken cancellationToken) => WriteNewRandomFileInfoAsync(writeContentAsync, extension, null, cancellationToken);
+
+    public Task<FileInfo> WriteNewRandomFileInfoAsync(Func<StreamWriter, CancellationToken, Task> writeContentAsync, Encoding? encoding, CancellationToken cancellationToken) => WriteNewRandomFileInfoAsync(writeContentAsync, null, encoding, cancellationToken);
+
+    public async Task<FileInfo> WriteNewRandomFileInfoAsync(Func<StreamWriter, CancellationToken, Task> writeContentAsync, string? extension, Encoding? encoding, CancellationToken cancellationToken)
+    {
+        FileInfo result;
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        }
+        else
+        {
+            if (extension[0] != '.') extension = $".{extension}";
+            result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+        }
+        cancellationToken.ThrowIfCancellationRequested();
+        if (encoding is null)
+            using (var writer = result.CreateText())
+            {
+                await writeContentAsync(writer, cancellationToken);
+                await writer.FlushAsync(cancellationToken);
+            }
+        else
+            using (var stream = result.Create())
+            {
+                using var writer = new StreamWriter(stream, encoding);
+                await writeContentAsync(writer, cancellationToken);
+                await writer.FlushAsync(cancellationToken);
+            }
+        result.Refresh();
+        return result;
+    }
+
+    public Task<FileInfo> NewRandomFileInfoAsync(Func<FileStream, CancellationToken, Task> writeContentAsync, CancellationToken cancellationToken) => NewRandomFileInfoAsync(writeContentAsync, null, cancellationToken);
+
+    public async Task<FileInfo> NewRandomFileInfoAsync(Func<FileStream, CancellationToken, Task> writeContentAsync, string? extension, CancellationToken cancellationToken)
+    {
+        FileInfo result;
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        }
+        else
+        {
+            if (extension[0] != '.') extension = $".{extension}";
+            result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+            while (result.Exists)
+                result = new(Path.Combine(_directory.FullName, $"{Guid.NewGuid():n}{extension}"));
+        }
+        using (var stream = result.Create())
+        {
+            await writeContentAsync(stream, cancellationToken);
+            await stream.FlushAsync(cancellationToken);
+        }
+        result.Refresh();
+        return result;
+    }
+
+    public DirectoryInfo NewRandomDirectoryInfo()
+    {
+        DirectoryInfo result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        while (result.Exists)
+            result = new(Path.Combine(_directory.FullName, Guid.NewGuid().ToString("n")));
+        result.Create();
+        result.Refresh();
+        return result;
+    }
 
     public TempStagingFolder()
     {
