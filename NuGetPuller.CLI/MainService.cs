0a5be5fba74330f4bf3904d329a3288d8663321b
erwinel <37079914+erwinel@users.noreply.github.com>
2023-12-18 19:44:48 +0000

    Made help methods synchronous

--- NuGetPuller.CLI/MainService.cs
+++ NuGetPuller.CLI/MainService.cs
@@ -17,7 +17,7 @@ public class MainService : BackgroundService
 
     public MainService(IOptions<AppSettings> options, ILogger<MainService> logger, IServiceProvider serviceProvider, IHostApplicationLifetime applicationLifetime) =>
         (_settings, _logger, _serviceProvider, _applicationLifetime) = (options.Value, logger, serviceProvider, applicationLifetime);
- 
+
     public static void CheckIgnoredDependentCommandLineArgument(bool value, Func<(string DependentSwitch, string SwitchName)> getSwitchNames)
     {
         if (!value)
@@ -25,7 +25,7 @@ public class MainService : BackgroundService
         (string dependentSwitch, string switchName) = getSwitchNames();
         WriteConsoleWarning("Command line switch {0} is ignored because {1} is not specified.", dependentSwitch, switchName);
     }
-    
+
     public static void CheckIgnoredDependentCommandLineArgument(string? value, Func<(string DependentSwitch, string SwitchName)> getSwitchNames)
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -33,7 +33,7 @@ public class MainService : BackgroundService
         (string dependentSwitch, string switchName) = getSwitchNames();
         WriteConsoleWarning("Command line switch {0} is ignored because {1} is not specified.", dependentSwitch, switchName);
     }
-    
+
     public static void CheckIgnoredDependentCommandLineArgument(string? value, Func<(string DependentSwitch, string SwitchName1, string SwitchName2)> getSwitchNames)
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -41,7 +41,7 @@ public class MainService : BackgroundService
         (string dependentSwitch, string switchName1, string switchName2) = getSwitchNames();
         WriteConsoleWarning("Command line switch {0} is ignored because neither {1}, nor {2} is specified.", dependentSwitch, switchName1, switchName2);
     }
-    
+
     public static void CheckIgnoredDependentCommandLineArgument(string? value, Func<(string DependentSwitch, string SwitchName1, string SwitchName2, string SwitchName3, string SwitchName4)> getSwitchNames)
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -49,43 +49,43 @@ public class MainService : BackgroundService
         (string dependentSwitch, string switchName1, string switchName2, string switchName3, string switchName4) = getSwitchNames();
         WriteConsoleWarning("Command line switch {0} is ignored because neither {1}, {2}, {3}, nor {4} is specified.", dependentSwitch, switchName1, switchName2, switchName3, switchName4);
     }
-    
-    private Task WriteHelpToConsole()
-    {
-        return Task.FromException(new NotImplementedException());
-    }
 
-    private Task WriteListLocalHelpToConsole()
-    {
-        return Task.FromException(new NotImplementedException());
-    }
-
-    private Task WriteCheckDependenciesHelpToConsole()
+    private void WriteHelpToConsole()
     {
         throw new NotImplementedException();
     }
 
-    private Task WriteCreateBundleHelpToConsole()
+    private void WriteListLocalHelpToConsole()
     {
         throw new NotImplementedException();
     }
 
-    private Task WriteDownloadHelpToConsole()
+    private void WriteCheckDependenciesHelpToConsole()
     {
         throw new NotImplementedException();
     }
 
-    private Task WriteRemoveHelpToConsole()
+    private void WriteCreateBundleHelpToConsole()
     {
         throw new NotImplementedException();
     }
 
-    private Task WriteAddPackageFilesHelpToConsole()
+    private void WriteDownloadHelpToConsole()
     {
         throw new NotImplementedException();
     }
 
-    private Task WriteExportMetaDataHelpToConsole()
+    private void WriteRemoveHelpToConsole()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void WriteAddPackageFilesHelpToConsole()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void WriteExportMetaDataHelpToConsole()
     {
         throw new NotImplementedException();
     }
@@ -125,7 +125,7 @@ public class MainService : BackgroundService
                 break;
         }
     }
-    
+
     private static bool ArePackageIdsValid(IEnumerable<string> packageIds)
     {
         using var enumerator = packageIds.GetEnumerator();
@@ -153,7 +153,7 @@ public class MainService : BackgroundService
         }
         return true;
     }
-    
+
     private static bool TryParseVersionStrings(string[] versionStrings, out NuGetVersion[] versions)
     {
         int count = versionStrings.Length;
@@ -183,7 +183,7 @@ public class MainService : BackgroundService
         }
         return true;
     }
-    
+
     private static async Task OnCheckDependenciesAsync(string[]? packageIds, string[]? versionStrings, bool noDownload, IServiceProvider serviceProvider, ILogger logger, CancellationToken cancellationToken)
     {
         if (packageIds is null)
@@ -199,7 +199,7 @@ public class MainService : BackgroundService
         }
         else
         {
-            packageIds = packageIds.Distinct(StringComparer.InvariantCultureIgnoreCase).ToArray();
+            packageIds = packageIds.Distinct(PackageIdentitifierComparer).ToArray();
             if (ArePackageIdsValid(packageIds))
             {
                 var localService = serviceProvider.GetRequiredService<ILocalNuGetFeedService>();
@@ -210,7 +210,7 @@ public class MainService : BackgroundService
                     else
                         await DownloadMissingDependenciesAsync(localService, serviceProvider.GetRequiredService<IUpstreamNuGetClientService>(), logger, packageIds, cancellationToken);
                 }
-                else if (TryParseVersionStrings(versionStrings.Distinct(StringComparer.InvariantCultureIgnoreCase).ToArray(), out NuGetVersion[] versions))
+                else if (TryParseVersionStrings(versionStrings.Distinct(PackageIdentitifierComparer).ToArray(), out NuGetVersion[] versions))
                 {
                     if (noDownload)
                         await CheckDependenciesAsync(localService, logger, packageIds, versions, cancellationToken);
@@ -219,7 +219,7 @@ public class MainService : BackgroundService
                 }
             }
             else if (versionStrings is not null)
-                _ = TryParseVersionStrings(versionStrings.Distinct(StringComparer.InvariantCultureIgnoreCase).ToArray(), out _);
+                _ = TryParseVersionStrings(versionStrings.Distinct(PackageIdentitifierComparer).ToArray(), out _);
         }
     }
 
@@ -234,7 +234,7 @@ public class MainService : BackgroundService
         }
         else if (versionStrings is null)
             await ExportBundleAsync(path, createFrom, saveTo, packageIds, serviceProvider.GetRequiredService<ILocalNuGetFeedService>(), logger, cancellationToken);
-        else if (TryParseVersionStrings(versionStrings.Distinct(StringComparer.InvariantCultureIgnoreCase).ToArray(), out NuGetVersion[] versions))
+        else if (TryParseVersionStrings(versionStrings.Distinct(PackageIdentitifierComparer).ToArray(), out NuGetVersion[] versions))
             await ExportBundleAsync(path, createFrom, saveTo, packageIds, versions, serviceProvider.GetRequiredService<ILocalNuGetFeedService>(), logger, cancellationToken);
     }
 
@@ -247,7 +247,7 @@ public class MainService : BackgroundService
     {
         throw new NotImplementedException("--remove not implemented.");
     }
-    
+
     private static Task<Task> OnAddPackageFiles(string[] paths, IServiceProvider serviceProvider, ILogger logger, CancellationToken cancellationToken)
     {
         throw new NotImplementedException("--add-file not implemented.");
@@ -295,7 +295,7 @@ public class MainService : BackgroundService
                         if (string.IsNullOrWhiteSpace(_settings.PackageId) && !string.IsNullOrWhiteSpace(_settings.Version))
                             WriteConsoleWarning("Command line switch {0} is ignored because {1} is not specified.", CommandLineSwitches.COMMAND_LINE_SWITCH_version,
                                 CommandLineSwitches.COMMAND_LINE_SWITCH_package_id);
-                        await WriteCheckDependenciesHelpToConsole();
+                        WriteCheckDependenciesHelpToConsole();
                     }
                     else
                         await OnCheckDependenciesAsync(_settings.PackageId.SplitIfNotWhiteSpace(','), _settings.Version.SplitIfNotWhiteSpace(','), _settings.NoDownload, scope.ServiceProvider, _logger, stoppingToken);
@@ -326,7 +326,7 @@ public class MainService : BackgroundService
                         CheckIgnoredDependentCommandLineArgument(_settings.CreateFrom, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_create_from, CommandLineSwitches.COMMAND_LINE_SWITCH_create_bundle));
                         CheckIgnoredDependentCommandLineArgument(_settings.SaveMetaDataTo, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_save_metadata_to, CommandLineSwitches.COMMAND_LINE_SWITCH_create_bundle));
                         if (_settings.Help)
-                            await WriteListLocalHelpToConsole();
+                            WriteListLocalHelpToConsole();
                         else
                             await WriteLocalPackagesToConsoleAsync(scope.ServiceProvider.GetRequiredService<ILocalNuGetFeedService>(), _settings.IncludeVersions, stoppingToken);
                     }
@@ -354,18 +354,18 @@ public class MainService : BackgroundService
                                     {
                                         if (!_settings.Help)
                                             WriteConsoleWarning("No valid command line arguments provided.");
-                                        await WriteHelpToConsole();
+                                        WriteHelpToConsole();
                                     }
                                     else if (_settings.Help)
-                                        await WriteExportMetaDataHelpToConsole();
+                                        WriteExportMetaDataHelpToConsole();
                                     else
-                                        await await OnExportMetaData(_settings.ExportMetaData, scope.ServiceProvider, _logger, stoppingToken);
+                                        await OnExportMetaData(_settings.ExportMetaData, scope.ServiceProvider, _logger, stoppingToken);
                                 }
                                 else if (!string.IsNullOrWhiteSpace(_settings.ExportMetaData))
                                     WriteConsoleError("Command line switch {0} cannot be used with {1}.", CommandLineSwitches.COMMAND_LINE_SWITCH_add_file,
                                         CommandLineSwitches.COMMAND_LINE_SWITCH_export_metadata);
                                 else if (_settings.Help)
-                                    await WriteAddPackageFilesHelpToConsole();
+                                    WriteAddPackageFilesHelpToConsole();
                                 else
                                     await OnAddPackageFiles(_settings.AddPackageFiles.Split(';'), scope.ServiceProvider, _logger, stoppingToken);
                             }
@@ -378,7 +378,7 @@ public class MainService : BackgroundService
                                 CheckIgnoredDependentCommandLineArgument(_settings.PackageId, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_package_id, CommandLineSwitches.COMMAND_LINE_SWITCH_check_depencencies,
                                     CommandLineSwitches.COMMAND_LINE_SWITCH_create_bundle));
                                 if (_settings.Help)
-                                    await WriteRemoveHelpToConsole();
+                                    WriteRemoveHelpToConsole();
                                 else
                                     await OnRemove(_settings.Remove.Split(','), _settings.Version.SplitIfNotWhiteSpace(','), _settings.SaveTo.NullIfWhiteSpace(), scope.ServiceProvider, _logger, stoppingToken);
                             }
@@ -395,7 +395,7 @@ public class MainService : BackgroundService
                             CheckIgnoredDependentCommandLineArgument(_settings.PackageId, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_package_id, CommandLineSwitches.COMMAND_LINE_SWITCH_check_depencencies,
                                 CommandLineSwitches.COMMAND_LINE_SWITCH_create_bundle));
                             if (_settings.Help)
-                                await WriteDownloadHelpToConsole();
+                                WriteDownloadHelpToConsole();
                             else
                                 await OnDownload(_settings.Download.Split(','), _settings.Version.SplitIfNotWhiteSpace(','), _settings.NoDependencies, _settings.SaveTo, scope.ServiceProvider, _logger, stoppingToken);
                         }
@@ -413,14 +413,14 @@ public class MainService : BackgroundService
                         CheckIgnoredDependentCommandLineArgument(_settings.NoDependencies, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_no_dependencies, CommandLineSwitches.COMMAND_LINE_SWITCH_download));
                         CheckIgnoredDependentCommandLineArgument(_settings.SaveTo, () => (CommandLineSwitches.COMMAND_LINE_SWITCH_save_to, CommandLineSwitches.COMMAND_LINE_SWITCH_remove));
                         if (_settings.Help)
-                            await WriteCreateBundleHelpToConsole();
+                            WriteCreateBundleHelpToConsole();
                         else
                             await OnCreateBundle(_settings.CreateBundle, _settings.CreateFrom.NullIfWhiteSpace(), _settings.SaveMetaDataTo.NullIfWhiteSpace(), _settings.PackageId.SplitIfNotWhiteSpace(','),
                                 _settings.Version.SplitIfNotWhiteSpace(','), scope.ServiceProvider, _logger, stoppingToken);
                     }
                 }
             }
-            
+
 
             // var packageIds = _settings.Remove?.Split(',').Select(s => s.Trim()).Where(s => s.Length > 0).Distinct(NoCaseComparer);
             // HashSet<PackageIdentity> deletedPackages = new(PackageIdentityComparer.Default);
